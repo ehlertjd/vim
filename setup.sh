4bac746724d702173682b70f618cc9c082d3a3a9
Dan Fredell <Dan.Fredell@gmail.com>
2020-01-09 13:30:29 -0600

    Update setup.sh
    Copy scripts dir, make swapfiles dir

--- setup.sh
+++ setup.sh
@@ -30,13 +30,17 @@ mkdir -p backup
 mkdir -p plugged
 mkdir -p autoload
 mkdir -p colors
+mkdir -p swapfiles
+mkdir -p scripts
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 vim +PlugInstall +qall
+cd -
 cd ~/.vim/plugged/YouCompleteMe
 ./install.sh --go-completer
+cd -
 cd ~/.vim/plugged/tern_for_vim
 npm install
-
+cd -
 curl -fLo ~/.vim/colors/srcery.vim https://raw.githubusercontent.com/srcery-colors/srcery-vim/master/colors/srcery.vim 
-
+cp ./scripts ~/.vim/scripts
