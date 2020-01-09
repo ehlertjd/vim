597e81182d6c7d74ae9039404ca9c1a8cae94d36
Dan Fredell <Dan.Fredell@gmail.com>
2020-01-09 12:00:34 -0600

    Update setup.sh
    Add a few checks to the setup

--- setup.sh
+++ setup.sh
@@ -1,3 +1,35 @@
+#!/usr/bin/env bash
+# Prereqs
+
+
+if [ ! -x  "$(command -v go)" ]; then 
+    echo you might want to install golang
+fi
+     
+if [ ! -x  "$(command -v npm)" ]; then 
+    echo you will need npm
+    exit 1
+fi
+     
+ if [ ! -x  "$(command -v apt-vim)" ]; then 
+    echo Installing apt-vim; 
+    curl -sL https://raw.githubusercontent.com/egalpin/apt-vim/master/install.sh | sh; 
+ fi;
+
+
+if [ ! -d ~/.vim/bundle/Vundle.vim ]; then 
+    echo installing Vundle
+    git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+fi
+
+if [ ! -d ~/.vim/plugged/YouCompleteMe ]; then
+    echo installing YouCompleteMe
+    git clone https://github.com/ycm-core/YouCompleteMe.git ~/.vim/plugged/YouCompleteMe
+    cd ~/.vim/plugged/YouCompleteMe
+    git submodule update --init --recursive
+    cd -
+fi
+
 ln -s ~/.vim/vimrc ~/.vimrc
 cd ~/.vim
 mkdir -p backup
