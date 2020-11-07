270df4780e9134300cf6a5a9087cdf568ca815c1
Justin Ehlert <ehlertjd@gmail.com>
2020-11-07 15:12:11 -0600

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,10 +1,11 @@
 ln -s ~/.vim/vimrc ~/.vimrc
 cd ~/.vim
 mkdir -p backup
+mkdir -p swapfiles
 mkdir -p plugged
 mkdir -p autoload
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 vim +PlugInstall +qall
 cd ~/.vim/plugged/YouCompleteMe
-./install.sh --go-completer
+./install.py --go-completer
